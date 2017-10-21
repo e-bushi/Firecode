40beccd1f800b854c4b0b148df5d88c3a0e23987
Chris Mauldin <Chrislovemauldin@gmail.com>
2017-10-21 11:16:00 -0800

    Write function that created fibonacci sequence recursively

---
+++ Firecode-fibonacciSeqRecur.playground/Contents.swift
@@ -0,0 +1,23 @@
+//: A UIKit based Playground for presenting user interface
+  
+import UIKit
+import PlaygroundSupport
+
+//Write a recursive method fib(n) that returns the nth Fibonacci number
+
+class MyViewController : UIViewController {
+    override func loadView() {
+        let view = UIView()
+        view.backgroundColor = .white
+
+        let label = UILabel()
+        label.frame = CGRect(x: 150, y: 200, width: 200, height: 20)
+        label.text = "Hello World!"
+        label.textColor = .black
+        
+        view.addSubview(label)
+        self.view = view
+    }
+}
+// Present the view controller in the Live View window
+PlaygroundPage.current.liveView = MyViewController()
