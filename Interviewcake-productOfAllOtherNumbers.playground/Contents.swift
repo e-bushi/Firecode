f1cf94e898906393f89c8e65e3a19f0ea49ebf73
Chris Mauldin <Chrislovemauldin@gmail.com>
2017-11-10 17:21:00 -0800

    Product of all numbers challenge

---
+++ Interviewcake-productOfAllOtherNumbers.playground/Contents.swift
@@ -0,0 +1,22 @@
+//: A UIKit based Playground for presenting user interface
+  
+import UIKit
+import PlaygroundSupport
+
+//write a function that takes a list as input and for each index find the product of all the numbers except the value of that index
+class MyViewController : UIViewController {
+    override func loadView() {
+        let view = UIView()
+        view.backgroundColor = .white
+
+        let label = UILabel()
+        label.frame = CGRect(x: 150, y: 200, width: 200, height: 20)
+        label.text = "Hello World!"
+        label.textColor = .black
+        
+        view.addSubview(label)
+        self.view = view
+    }
+}
+// Present the view controller in the Live View window
+PlaygroundPage.current.liveView = MyViewController()
